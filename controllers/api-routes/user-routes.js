06792e86e84c1cec89fd622ba22e6a4c2d1a4d37
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-02 09:39:35 -1000

    add DELETE /api/users/:id route

--- controllers/api-routes/user-routes.js
+++ controllers/api-routes/user-routes.js
@@ -69,6 +69,23 @@ router.put("/:id", (req, res) => {
 });
 
 // DELETE /api/users/:id
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with that ID" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
