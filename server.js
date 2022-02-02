c8b65adae8647e63259fd57aa3e16fa98335cddb
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-01 17:16:52 -1000

    setup basic express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const sequelize = require("./config/connection");
+const path = require("path");
+const PORT = process.env.PORT | 3001;
+const routes = require("./controllers");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
