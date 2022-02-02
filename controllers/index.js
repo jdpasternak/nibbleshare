6d83ec2d03346e6524cfe1f3ba431335eb2ade5c
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-02 10:37:21 -1000

    setup handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const apiRoutes = require("./api-routes");
+const homeRoutes = require("./home-routes");
 
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
