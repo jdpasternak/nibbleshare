092541a20e1045ac1f0c4126902f24f0c16d6ca1
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-02 08:51:22 -1000

    create post model

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("sequelize");
+const User = require("./User");
+const markdown = require("markdown-js");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3],
+      },
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate(newPostData) {
+        newPostData.content = markdown.makeHtml(content);
+        return newPostData;
+      },
+      beforeUpdate(updatedPostData) {
+        updatedPostData.content = markdown.makeHtml(content);
+        return updatedPostData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
