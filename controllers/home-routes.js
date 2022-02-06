b2463829bba888d571f895c33427bc0c813b989a
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-05 21:16:04 -1000

    Check for logged in to display login/logout options

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
     },
   }).then((dbPostData) => {
     const posts = dbPostData.map((post) => post.get({ plain: true }));
-    res.render("homepage", { posts });
+    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
   });
 });
 
@@ -81,7 +81,7 @@ router.get("/dashboard", withAuth, (req, res) => {
     .then((dbPostData) => {
       console.log(dbPostData);
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("dashboard", { posts });
+      res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
     })
     .catch((err) => {
       console.log(err);
